0a07cde14dd1a3ad21dc13a436a3d4c0b6731d2e
Joel Kuehne <joelckuehne@gmail.com>
2022-02-11 13:00:29 -0600

    Started PID Programmign

--- src/main/cpp/Drive.cpp
+++ src/main/cpp/Drive.cpp
@@ -2,8 +2,10 @@
 
 extern WiringDiagram MyWiringDiagram;
 
-Drive::Drive() : MecanumDrive (*frontLeft, *backLeft, *frontRight, *backRight)
+Drive::Drive() : MecanumDrive (*frontLeft, *backLeft, *frontRight, *backRight), DrivePID("Drive", DrivePIDValues)
 {
+    InitPIDValues();
+
     frontLeft = new ctre::phoenix::motorcontrol::can::WPI_VictorSPX (MyWiringDiagram.c_FrontLeftMotor);
     frontRight = new ctre::phoenix::motorcontrol::can::WPI_VictorSPX (MyWiringDiagram.c_FrontRightMotor);
     backLeft = new ctre::phoenix::motorcontrol::can::WPI_VictorSPX (MyWiringDiagram.c_BackLeftMotor);
@@ -13,4 +15,16 @@ Drive::Drive() : MecanumDrive (*frontLeft, *backLeft, *frontRight, *backRight)
     frontRight->SetInverted(true);
     backLeft->SetInverted(true);
     backRight->SetInverted(true);
+}
+
+void Drive::RunPIDControl()
+{
+
+}
+
+void Drive::InitPIDValues()
+{
+    DrivePIDValues.kP = 6e-5, DrivePIDValues.kI = 1e-6, DrivePIDValues.kD = 0, DrivePIDValues.kMaxOutput = 1.0, DrivePIDValues.kMinOutput = -1.0;
+    DrivePIDValues.setpoint = 0;
+    DrivePIDValues.positionTolerance = 1, DrivePIDValues.velocityTolerance = 1;
 }
--- src/main/include/Drive.h
+++ src/main/include/Drive.h
@@ -2,15 +2,23 @@
 
 #include <frc/drive/MecanumDrive.h>
 #include <ctre/phoenix/motorcontrol/can/WPI_VictorSPX.h>
+#include "FrcPID.h"
 #include "WiringDiagram.h"
 
 class Drive : public frc::MecanumDrive {
+ private:
+  void InitPIDValues();
+
  public:
   Drive();
+
+  FrcPID DrivePID;
+  FrcPID::PIDValues DrivePIDValues;
 
   ctre::phoenix::motorcontrol::can::WPI_VictorSPX* frontLeft;
   ctre::phoenix::motorcontrol::can::WPI_VictorSPX* frontRight;
   ctre::phoenix::motorcontrol::can::WPI_VictorSPX* backLeft;
   ctre::phoenix::motorcontrol::can::WPI_VictorSPX* backRight;
 
+  void RunPIDControl();
 };
