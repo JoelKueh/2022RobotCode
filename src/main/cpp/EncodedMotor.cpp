e4283e1e34cbb38cf36f096da8be86514c0137f9
Joel Kuehne <joelckuehne@gmail.com>
2022-02-11 13:00:08 -0600

    Moved PIDValues into a struct

--- src/main/cpp/EncodedMotor.cpp
+++ src/main/cpp/EncodedMotor.cpp
@@ -6,49 +6,61 @@
 /**
  * Constructs a SparkMax EncodedMotor
  * */
-EncodedMotor::EncodedMotor(char inputName[], int canID, MotorType motorType) :
+EncodedMotor::EncodedMotor(char inputName[], int canID, MotorType motorType, PIDValues inputValues) :
     rev::CANSparkMax(canID, motorType),
     PID (GetPIDController()),
     Encoder (GetEncoder())
 {
     motorName = inputName;
+
+    inputPIDValues = inputValues;
+    myPIDValues = inputValues;
+
+    // kP = 6e-5, kI = 1e-6, kD = 0, kIz = 0, kFF = 0.000015, kMaxOutput = 1.0, kMinOutput = -1.0;
+    // P = 6e-5, I = 1e-6, D = 0, Iz = 0, FF = 0.000015, MaxOutput = 1.0, MinOutput = -1.0;
 }
 
-EncodedMotor::EncodedMotor(char inputName[], int canID, MotorType motorType, int countsPerRev) :
+EncodedMotor::EncodedMotor(char inputName[], int canID, MotorType motorType, int countsPerRev, PIDValues inputValues) :
     rev::CANSparkMax(canID, motorType),
     PID (GetPIDController()),
     Encoder (GetEncoder(rev::SparkMaxRelativeEncoder::Type::kQuadrature, countsPerRev))
 {
     motorName = inputName;
+
+    inputPIDValues = inputValues;
+    myPIDValues = inputValues;
+
+    // kP = 6e-5, kI = 1e-6, kD = 0, kIz = 0, kFF = 0.000015, kMaxOutput = 1.0, kMinOutput = -1.0;
+    // P = 6e-5, I = 1e-6, D = 0, Iz = 0, FF = 0.000015, MaxOutput = 1.0, MinOutput = -1.0;
 }
 
 /**
  * This function sends the PID values to the smartdashboard
  * */
-void EncodedMotor::InitSmartDashboardPID()
+void EncodedMotor::InitSmartDashboard()
 {
-    frc::SmartDashboard::PutNumber(motorName + "P Gain", kP);
-    frc::SmartDashboard::PutNumber(motorName + "I Gain", kI);
-    frc::SmartDashboard::PutNumber(motorName + "D Gain", kD);
-    frc::SmartDashboard::PutNumber(motorName + "I Zone", kIz);
-    frc::SmartDashboard::PutNumber(motorName + "Feed Forward", kFF);
-    frc::SmartDashboard::PutNumber(motorName + "Max Output", kMaxOutput);
-    frc::SmartDashboard::PutNumber(motorName + "Min Output", kMinOutput);
+    frc::SmartDashboard::PutNumber(motorName + "P Gain", myPIDValues.kP);
+    frc::SmartDashboard::PutNumber(motorName + "I Gain", myPIDValues.kI);
+    frc::SmartDashboard::PutNumber(motorName + "D Gain", myPIDValues.kD);
+    frc::SmartDashboard::PutNumber(motorName + "I Zone", myPIDValues.kIz);
+    frc::SmartDashboard::PutNumber(motorName + "Feed Forward", myPIDValues.kFF);
+    frc::SmartDashboard::PutNumber(motorName + "Max Output", myPIDValues.kMaxOutput);
+    frc::SmartDashboard::PutNumber(motorName + "Min Output", myPIDValues.kMinOutput);
 
-    frc::SmartDashboard::PutNumber(motorName + "SetPoint", SetPoint);
+    frc::SmartDashboard::PutNumber(motorName + "SetPoint", myPIDValues.setpoint);
 }
 
 /**
  * Sends the Velocity to SmartDashboard
  * */
-void EncodedMotor::PeriodicSmartDashboardPID()
+void EncodedMotor::PeriodicSmartDashboard()
 {
     frc::SmartDashboard::PutNumber(motorName + "Velocity", Encoder.GetVelocity());
 }
 
 void EncodedMotor::PutSetpoint()
 {
-    frc::SmartDashboard::PutNumber(motorName + "SetPoint", SetPoint);
+    frc::SmartDashboard::PutNumber(motorName + "SetPoint", myPIDValues.setpoint);
 }
 
 /**
@@ -56,23 +68,23 @@ void EncodedMotor::PutSetpoint()
  * */
 void EncodedMotor::GetSmartDashboard()
 {
-    P = frc::SmartDashboard::GetNumber(motorName + "P Gain", 0);
-    I = frc::SmartDashboard::GetNumber(motorName + "I Gain", 0);
-    D = frc::SmartDashboard::GetNumber(motorName + "D Gain", 0);
-    Iz = frc::SmartDashboard::GetNumber(motorName + "I Zone", 0);
-    FF = frc::SmartDashboard::GetNumber(motorName + "Feed Forward", 0);
-    MaxOutput = frc::SmartDashboard::GetNumber(motorName + "Max Output", 0);
-    MinOutput = frc::SmartDashboard::GetNumber(motorName + "Min Output", 0);
+    inputPIDValues.kP = frc::SmartDashboard::GetNumber(motorName + "P Gain", 0);
+    inputPIDValues.kI = frc::SmartDashboard::GetNumber(motorName + "I Gain", 0);
+    inputPIDValues.kD = frc::SmartDashboard::GetNumber(motorName + "D Gain", 0);
+    inputPIDValues.kIz = frc::SmartDashboard::GetNumber(motorName + "I Zone", 0);
+    inputPIDValues.kFF = frc::SmartDashboard::GetNumber(motorName + "Feed Forward", 0);
+    inputPIDValues.kMaxOutput = frc::SmartDashboard::GetNumber(motorName + "Max Output", 0);
+    inputPIDValues.kMinOutput = frc::SmartDashboard::GetNumber(motorName + "Min Output", 0);
 
-    if(P != kP) { kP = P; PID.SetP(kP); }
-    if(I != kI) { kI = I; PID.GetI(kI); }
-    if(D != kD) { kD = D; PID.SetD(kD); }
-    if(Iz != kIz) { kIz = Iz; PID.SetIZone(kIz); }
-    if(MaxOutput != kMaxOutput || MinOutput != kMinOutput)
+    if(inputPIDValues.kP != myPIDValues.kP) { myPIDValues.kP = inputPIDValues.kP; PID.SetP(myPIDValues.kP); }
+    if(inputPIDValues.kI != myPIDValues.kI) { myPIDValues.kI = inputPIDValues.kI; PID.SetI(myPIDValues.kI); }
+    if(inputPIDValues.kD != myPIDValues.kD) { myPIDValues.kD = inputPIDValues.kD; PID.SetD(myPIDValues.kD); }
+    if(inputPIDValues.kIz != myPIDValues.kIz) { myPIDValues.kIz = inputPIDValues.kIz; PID.SetIZone(myPIDValues.kIz); }
+    if(inputPIDValues.kMaxOutput != myPIDValues.kMaxOutput || inputPIDValues.kMinOutput != myPIDValues.kMinOutput)
     {
-        kMaxOutput = MaxOutput;
-        kMinOutput = MinOutput;
-        PID.SetOutputRange(kMinOutput, kMaxOutput);
+        myPIDValues.kMaxOutput = inputPIDValues.kMaxOutput;
+        myPIDValues.kMinOutput = inputPIDValues.kMinOutput;
+        PID.SetOutputRange(myPIDValues.kMinOutput, myPIDValues.kMaxOutput);
     }
 }
 
@@ -81,6 +93,6 @@ void EncodedMotor::GetSmartDashboard()
  * */
 void EncodedMotor::RunPIDFromSmartDashboard()
 {
-    SetPoint = frc::SmartDashboard::GetNumber(motorName + "SetPoint", 0);
-    if(LastSetPoint != SetPoint) { PID.SetReference(SetPoint, rev::ControlType::kVelocity); }
+    inputPIDValues.setpoint = frc::SmartDashboard::GetNumber(motorName + "SetPoint", 0);
+    if(inputPIDValues.setpoint != myPIDValues.setpoint) { myPIDValues.setpoint = inputPIDValues.setpoint; PID.SetReference(myPIDValues.setpoint, rev::ControlType::kVelocity); }
 }
--- src/main/include/EncodedMotor.h
+++ src/main/include/EncodedMotor.h
@@ -5,23 +5,32 @@
 #include "WiringDiagram.h"
 
 class EncodedMotor : public rev::CANSparkMax {
- private:
+ public:
   rev::SparkMaxPIDController PID;
   rev::SparkMaxRelativeEncoder Encoder;
 
-  
-  double kP = 6e-5, kI = 1e-6, kD = 0, kIz = 0, kFF = 0.000015, kMaxOutput = 1.0, kMinOutput = -1.0;
-  double P = 6e-5, I = 1e-6, D = 0, Iz = 0, FF = 0.000015, MaxOutput = 1.0, MinOutput = -1.0;
-  const double MaxRPM = 5700;
-
-  double SetPoint = 0;
-  double LastSetPoint = 0;
-
   std::string motorName;
+  
+  struct PIDValues
+  {
+    double kP = 6e-5, kI = 1e-6, kD = 0, kIz = 0, kFF = 0.000015, kMaxOutput = 1.0, kMinOutput = -1.0;
 
- public:
-  EncodedMotor(char inputName[], int canID, rev::CANSparkMax::MotorType motorType);
-  EncodedMotor(char inputName[], int canID, rev::CANSparkMax::MotorType motorType, int countsPerRev);
+    double setpoint = 0;
+    double positionTolerance = 1, velocityTolerance = 1;
+  };
+
+  PIDValues myPIDValues;
+  PIDValues inputPIDValues;
+
+  // double kP = 6e-5, kI = 1e-6, kD = 0, kIz = 0, kFF = 0.000015, kMaxOutput = 1.0, kMinOutput = -1.0;
+  // double P = 6e-5, I = 1e-6, D = 0, Iz = 0, FF = 0.000015, MaxOutput = 1.0, MinOutput = -1.0;
+  // const double MaxRPM = 5700;
+
+  // double SetPoint = 0;
+  // double LastSetPoint = 0;
+
+  EncodedMotor(char inputName[], int canID, rev::CANSparkMax::MotorType motorType, PIDValues inputValues);
+  EncodedMotor(char inputName[], int canID, rev::CANSparkMax::MotorType motorType, int countsPerRev, PIDValues inputValues);
 
   void PutSetpoint();
   void InitSmartDashboard();
